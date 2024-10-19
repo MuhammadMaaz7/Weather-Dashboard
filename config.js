f23ed813fea7c4467b808f9424f939ec8f209394
MuhammadMaaz7 <121642049+MuhammadMaaz7@users.noreply.github.com>
2024-10-19 23:42:09 +0500

    Add config.js with API keys

---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+    OPENWEATHER_API_KEY: '84568c9d8b613ce5b360e349ac37a7a2',
+    GEMINI_API_KEY: 'AIzaSyBq1LA6tcA1GC3jKmUDqUw2gCoCOSKDQt4',
+    OPENWEATHER_BASE_URL: 'https://api.openweathermap.org/data/2.5',
+    GEMINI_API_URL: 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent'
+};
